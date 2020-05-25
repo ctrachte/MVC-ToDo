2c78ef56951a64059abdcf335e06468666969a4d
ctrachte <cetrachte@hotmail.com>
2020-05-25 13:19:15 -0500

    fix error handling routing

--- Startup.cs
+++ Startup.cs
@@ -51,7 +51,7 @@ namespace MvcToDo
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/ToDo/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
