1b98a1d5468294fbc0da813151127e5e6c299398
ctrachte <cetrachte@hotmail.com>
2020-06-03 15:31:38 -0500

    add validation to model

--- Models/ToDo.cs
+++ Models/ToDo.cs
@@ -6,10 +6,17 @@ namespace MvcToDo.Models
     public class ToDo
     {
         public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; }
+
         [DataType(DataType.Date)]
         public DateTime? CreatedDate { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime? CompletedDate { get; set; }
+
         public bool Complete { get; set; }
     }
 }
